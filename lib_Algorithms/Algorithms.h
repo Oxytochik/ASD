751592427004a328d62ee6993898db9ed8dfb866
oxy <oxyrvin@gmail.com>
2025-10-25 23:09:30 +0300

    Add expression_check and test for it

--- lib_Algorithms/Algorithms.h
+++ lib_Algorithms/Algorithms.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <initializer_list>
 #include "../lib_Stack/stack.h"
 
 int BracketsNumb(const std::string input) {
@@ -34,4 +35,99 @@ bool CheckBrackets(const std::string input) {
 		}
 	}
 	return string.is_empty();
+}
+
+bool is_in(const char element, char arr[], int size) {
+	
+	for (int i = 0; i<size; i++){
+		if (element == arr[i]) {
+			return true;
+		}
+	}
+	return false;
+}
+
+void read_expression(const std::string expression) {
+	int size = expression.size();
+	Stack<char> breckets(size);
+	Stack<char> oper_numb(size);
+	bool space = false;
+	char numbers[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+	char operators[5] = { '*', '+', '-', '/', '^' };
+	char alphabet[3] = { 'x', 'y', 'z' };
+
+	for (char element : expression) {
+		if (element == ' ') {
+			space = true;
+			continue;
+		}
+		//breckets block
+		if (element == '(') {
+			if (!oper_numb.is_empty() && (is_in(oper_numb.top(), numbers, 10) || is_in(oper_numb.top(), alphabet, 3))) {
+				throw std::logic_error("Uncorrect position of operand and open breckets");
+			}
+			breckets.push(element);
+			space = false;
+			continue;
+		}
+		if (element == ')') {
+			if (breckets.is_empty()) {
+				throw std::logic_error("For close bracket doesnt exist open bracket");
+			}
+			else {
+				if (is_in(oper_numb.top(), operators, 5)) {
+					throw std::logic_error("Close breckets after operation symbol");
+				}
+				else {
+					char last_element = breckets.top();
+					breckets.pop();
+					oper_numb.push(')');
+					space = false;
+					continue;
+				}
+				
+
+			}
+		}
+		//number and opearatin block
+
+		if (is_in(element, numbers, 10) || is_in(element, alphabet, 3)) {
+			if ((oper_numb.is_empty() || is_in(oper_numb.top(), operators, 5)) 
+				|| 
+				(is_in(oper_numb.top(), numbers, 10) && space == false && (!is_in(element, alphabet, 3)))) 
+			{
+				oper_numb.push(element);
+				space = false;
+				continue;
+			}
+			else {
+				throw std::logic_error("Incorrect pos of operor");
+			}
+		}
+		if (is_in(element, operators, 5)) {
+			if (oper_numb.is_empty()) {
+				throw std::logic_error("Operation symbol cant be first");
+			}
+			else if (is_in(oper_numb.top(), numbers, 10) || is_in(oper_numb.top(), alphabet, 3) || (oper_numb.top() == ')')) {
+				oper_numb.push(element);
+				space = false;
+				continue;
+			}
+			else {
+				throw std::logic_error("Uncorrect operator position");
+			}
+
+		}
+		throw std::logic_error("Uncorrect symbol");
+		
+	}
+	if (oper_numb.is_empty()) {
+		throw std::logic_error("Empty expression");
+	}
+	if (!breckets.is_empty()) {
+		throw std::logic_error("Unclosed brackets");
+	}
+	if (is_in(oper_numb.top(), operators, 5)) {
+		throw std::logic_error("Operation symbol cant be last symbol");
+	}
 }
--- tests/test_Algorithms.cpp
+++ tests/test_Algorithms.cpp
@@ -1,15 +1,101 @@
 #include <gtest/gtest.h>
 #include "../lib_Algorithms/Algorithms.h"
 
+TEST(AlgorithmTests, BracketsCount) {
+	EXPECT_EQ(BracketsNumb("(("), 2);
+	EXPECT_EQ(BracketsNumb(" "), 0);
+	EXPECT_EQ(BracketsNumb("abc"), 0);
+	EXPECT_EQ(BracketsNumb("){["), 2);
+}
+
 TEST(AlgorithmTests, BracketBalance) {
 	std::string tested_string = "()()";
 	EXPECT_TRUE(CheckBrackets(tested_string));
 	EXPECT_FALSE(CheckBrackets(")"));
+	EXPECT_FALSE(CheckBrackets("{"));
 	EXPECT_FALSE(CheckBrackets("(("));
 	EXPECT_TRUE(CheckBrackets("([{}])"));
 	EXPECT_TRUE(CheckBrackets(""));
+}
 
-
-
+TEST(AlgorithmTests, Check_is_in) {
+	char tested_array[2] = {'a', '4'};
+	EXPECT_TRUE(is_in('a', tested_array, 2));
+	EXPECT_TRUE(is_in('4', tested_array, 2));
+	EXPECT_FALSE(is_in('c', tested_array, 2));
 
 }
+
+TEST(AlgotithmTests, CorrectExpression) {
+	EXPECT_NO_THROW(read_expression("(x +(5-((7/y)*10)))^z"));
+}
+
+TEST(AlgotithmTests, Brackets_after_operation) {
+	EXPECT_ANY_THROW(read_expression("x()"));
+}
+
+TEST(AlgotithmTests, Brackets_after_operation_numb_version) {
+	EXPECT_ANY_THROW(read_expression("5()"));
+}
+
+TEST(AlgotithmTests, CloseBracket_without_open_bracket) {
+	EXPECT_ANY_THROW(read_expression(" x + y)"));
+}
+
+TEST(AlgotithmTests, CloseBracket_after_operation) {
+	EXPECT_ANY_THROW(read_expression("( x + ) y"));
+}
+
+TEST(AlgotithmTests, Open_bracket_without_close_bracket) {
+	EXPECT_ANY_THROW(read_expression(" ((x + y)+8"));
+}
+
+TEST(AlgotithmTests, Number_after_number) {
+	EXPECT_ANY_THROW(read_expression("(5 5)"));
+}
+
+TEST(AlgotithmTests, First_operator) {
+	EXPECT_ANY_THROW(read_expression("+(5-5)"));
+}
+
+TEST(AlgotithmTests, Two_operation) {
+	EXPECT_ANY_THROW(read_expression("5 ++ 5"));
+}
+
+TEST(AlgotithmTests, Operation_after_brackets) {
+	EXPECT_ANY_THROW(read_expression("4 -(+5)"));
+}
+
+TEST(AlgotithmTests, Other_Symbol) {
+	EXPECT_ANY_THROW(read_expression("5 + a"));
+}
+
+TEST(AlgotithmTests, Empty_Expression) {
+	EXPECT_ANY_THROW(read_expression("()"));
+}
+
+TEST(AlgotithmTests, Operation_Symbol_Last) {
+	EXPECT_ANY_THROW(read_expression("5 + "));
+}
+
+TEST(AlgotithmTests, Operator_First) {
+	EXPECT_ANY_THROW(read_expression("+5"));
+}
+
+TEST(AlgotithmTests, Numb_and_Alphabet_Symbol_ver_1) {
+	EXPECT_ANY_THROW(read_expression("5y"));
+}
+
+TEST(AlgotithmTests, Numb_and_Alphabet_Symbol_ver_2) {
+	EXPECT_ANY_THROW(read_expression("z5"));
+}
+
+TEST(AlgotithmTests, One_Numb) {
+	EXPECT_NO_THROW(read_expression("5"));
+}
+
+
+
+
+
+
